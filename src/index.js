ee56557ba7a3323d9e3a62b4feebbe8e59452c37
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2019-10-25 18:38:34 +0530

    Add Buttons to access game history

--- src/index.js
+++ src/index.js
@@ -85,6 +85,7 @@ class Game extends React.Component {
     }));
   };
 
+  jumpTo = () => {};
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
@@ -94,6 +95,14 @@ class Game extends React.Component {
       ? `Winner: ${winner}`
       : `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move # ${move}` : `Go to game start`;
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
     return (
       <div className="game">
         <div className="game-board">
@@ -104,7 +113,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
